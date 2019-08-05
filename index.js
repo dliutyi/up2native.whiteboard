b4d3ab0089131af9648bf7b2e52cb1b8d94d2253
Dmytro <ljutyjj.dima@gmail.com>
2019-08-05 06:08:08 +0300

    first pass of whiteboard

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('redraw', function(msg){
+        io.emit('redraw', msg);
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(8081, '192.168.0.114', function(){
+    console.log('listening on http://192.168.0.114:8081');
+});
